87f1866ddea217aba98c29be998aba83bcbfa5b6
Soumaya-Qilin <127314619+kaguyami@users.noreply.github.com>
2023-09-24 20:10:10 +0200

    item component

---
+++ src/List/Item/index.js
@@ -0,0 +1,18 @@
+import './index.scss'
+import { formatPopulation } from './formatPopulation'
+
+export const Item = ({ nom, codesPostaux, codeRegion, population, populationIsFormatted }) => {
+
+    // J'affiche toutes les informations d'une commune en particulier
+    return (
+        <li className="item">
+            <span className="item-part"><strong>{nom}</strong> - {codeRegion}</span>
+            <span className="item-part">
+                <span className="item-label">Code postal :</span> {codesPostaux[0]}
+            </span>
+            <span className="item-part">
+                <span className="item-label">Population :</span> {populationIsFormatted === false ? population : formatPopulation(population)}
+            </span>
+        </li>
+    )
+}
